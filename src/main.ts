0c92874a3edf3323e5f5adfebf1383d1853a8bce
nictoro <nicholas.biantoro@ruangguru.com>
2022-05-28 11:42:36 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#canvas") as HTMLCanvasElement,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
